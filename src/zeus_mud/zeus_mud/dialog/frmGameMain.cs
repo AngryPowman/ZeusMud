aae61639762b3c301d5535c121e2b0111ac780f6
wjk <478286460@qq.com>
2013-10-16 12:42:10 +0800

    界面修改
    Signed-off-by: wjk <478286460@qq.com>

--- src/zeus_mud/zeus_mud/dialog/frmGameMain.cs
+++ src/zeus_mud/zeus_mud/dialog/frmGameMain.cs
@@ -15,6 +15,36 @@ namespace zeus_mud.dialog
         public frmGameMain()
         {
             InitializeComponent();
+            
+            txtEmail.Text = "478286460@qq.com";
+            txtLevel.Text = "100000";
+            txtName.Text = "猫仔";
+            txtAEmail.Text = "不对外显示";
+            txtALevel.Text = "-10";
+            txtAName.Text = "阿超";
+            lvRooms.Items.Add("万俊康");
+            lvRooms.Items[0].StateImageIndex = 0;
+            lvRooms.Items[0].SubItems.Add("1/2");
+            lvRooms.Items[0].SubItems.Add("10");
+            lvRooms.Items[0].SubItems.Add("1000000");
+            lvRooms.Items.Add("李狗蛋");
+            lvRooms.Items[0].StateImageIndex = 0;
+            lvRooms.Items[0].SubItems.Add("1/2");
+            lvRooms.Items[0].SubItems.Add("50");
+            lvRooms.Items[0].SubItems.Add("10");
+            for (int i = 1; i <= 10; i++)
+            {
+                lvRooms.Items.Add("李老板" + i.ToString());
+                lvRooms.Items[i].StateImageIndex = 0;
+                lvRooms.Items[i].SubItems.Add(i.ToString() + "/" + (i+1).ToString());
+                lvRooms.Items[i].SubItems.Add("250");
+                lvRooms.Items[i].SubItems.Add(i.ToString());
+            }
+        }
+
+        private void lvRooms_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
