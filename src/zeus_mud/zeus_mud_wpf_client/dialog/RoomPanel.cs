5710eda4de3103e491c6a868ddb6d800ebec31a7
iceable <iceable@foxmail.com>
2013-11-25 08:50:33 +0800

    pull前保存一下

---
+++ src/zeus_mud/zeus_mud_wpf_client/dialog/RoomPanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zeus_mud_wpf_client.dialog
+{
+    public partial class RoomPanel : UserControl
+    {
+        public RoomPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
